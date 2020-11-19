6f6237095da02d384a1c7e249cef38ceed22fd26
W-L <weilguny.lukas@gmail.com>
2020-11-19 11:19:47 +0000

    Update sra_dump.sh

--- sra_dump.sh
+++ sra_dump.sh
@@ -1,3 +1,3 @@
 # sensible arguments to download nanopore data. Because I can never remember them..
 prefetch SRA
-fastq-dump --gzip --readids --skip-technical --read-filter pass --clip $1 SRA/SRA.sra
+fastq-dump --gzip --readids --skip-technical --read-filter pass --clip SRA/SRA.sra
